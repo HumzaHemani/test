446bc304ad6d5f32a94fe670f42194f754e2110c
JeffreyCifello <jcifello1@gmail.com>
2021-03-17 15:25:10 -0400

    Update call_step6.sh

--- step6/call_step6.sh
+++ step6/call_step6.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 # run on R/3.6
 
-# Rscript step6.R BL5481 \
-# /home/cifellojp/step5_outs/mutations_NoIntervals \
-# /home/cifellojp/step5_in/10x_ref \
-# /data/TCR/10X_Genomics/Software/functotator/funcotator_dataSources.v1.6.20190124s \
-# ./ \
-# ./step6_out
-
-
 Rscript step6.R BL5481 \
-/home/cifellojp/step5/step5_outs/mutations_NoIntervals \
-/home/cifellojp/step5/step5_in/10x_ref \
-./test_funcotator \
+/pipeline/step5/step5_outs/mutations_NoIntervals \
+/pipeline/step5/step5_in/10x_ref \
+./funcotator \
 ./ \
 ./step6_out
