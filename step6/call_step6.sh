81078f5f4382d8ad9287e3087fcf589a666d6ce5
JeffreyCifello <jcifello1@gmail.com>
2021-03-17 15:27:00 -0400

    Update call_step6.sh

--- step6/call_step6.sh
+++ step6/call_step6.sh
@@ -2,8 +2,8 @@
 # run on R/3.6
 
 Rscript step6.R BL5481 \
-/pipeline/step5/step5_outs/mutations_NoIntervals \
-/pipeline/step5/step5_in/10x_ref \
+~/pipeline/step5/step5_outs/mutations_NoIntervals \
+~/pipeline/step5/step5_in/10x_ref \
 ./funcotator \
 ./ \
 ./step6_out
