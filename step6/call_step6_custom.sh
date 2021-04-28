67ff2bd4879d555ee0c4b8ca982fd35d9cbe52ba
JeffreyCifello <jcifello1@gmail.com>
2021-04-28 09:38:12 -0400

    Update call_step6_custom.sh

--- step6/call_step6_custom.sh
+++ step6/call_step6_custom.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-Rscript /step6.R $1 \
-$2 \
-$3 \
-$4 \
-$5 \
-$6
+sample=${sample:default_sample_num}
+scSNPs=${scSNPs:def_snp_dir}
+ref_10x=${ref_10x:def_ref_10x}
+funcotator_dir=${funcotator_dir:def_funcotator}
+scripts_dir=${scripts_dir:def_scripts_dir}
+out_dir=${out_dir:def_out_dir}
 
-for i in $(ls ${5}/annotated/VARIANTS*bash); do
+while [ $# -gt 0 ]; do            
+    if [[ $1 == *"--"* ]]; then
+      param="${1/--/}"
+      declare $param="$2"
+      echo $1 $2 #// Optional to see the parameter:value result
+    fi       
+  shift
+done
+
+
+Rscript /step6.R $sample \
+$scSNPs \
+$ref_10x \
+$funcotator_dir \
+$scripts_dir \
+$out_dir
+
+for i in $(ls ${scripts_dir}/annotated/VARIANTS*bash); do
   bash $i
 done
