e2b02debe044c2d1078f9d5d2e08b0233fc23d46
JeffreyCifello <jcifello1@gmail.com>
2021-05-04 15:28:40 -0400

    Update step5.R

--- step5/step5.R
+++ step5/step5.R
@@ -122,7 +122,7 @@ LEN=$(wc -w < ${DIR_SCRIPT}/TL/TL_${SAMPLE}_${TL})
 
 for i in $( seq 1 ${LEN} ); do
 TAG=`awk "NR==$i" ${DIR_SCRIPT}/TL/TL_${SAMPLE}_${TL}`
-VART=$(cat .tmp/${SAMPLE}_${TAG}_var.vcf | wc -l)
+VART=$(cat /.tmp/${SAMPLE}_${TAG}_var.vcf | wc -l)
 VAR=$(cat  ${DIR_O}/${SAMPLE}_${TAG}_var_FLTR.vcf | grep "PASS" | wc -l)
 READ_SAM=$(samtools view -c -F 4 ${scBAM}/${SAMPLE}_${TAG}.bam)
 UMI=$(samtools view  ${scBAM}/${SAMPLE}_${TAG}.bam | grep -o \'UB:............\' | grep -o \'..........$\' | uniq -c | wc -l)
