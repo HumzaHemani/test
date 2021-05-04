f9b281e6e40f7025bb61c3c1792ff5bbbe1e7203
JeffreyCifello <jcifello1@gmail.com>
2021-05-04 15:35:26 -0400

    Update step5.R

--- step5/step5.R
+++ step5/step5.R
@@ -71,7 +71,7 @@ REF_VAR=',snp_out,'
 
 # COPY BAM/REFERENCE FILE TO LOCAL SCRATCH:
 
-mkdir /.tmp
+mkdir .tmp
 
 # READ IN TAGS - PIPE INTO GNU PARALLEL :
 #						SPLIT BAM INTO SINGLE CELLS > CALL VARIANTS > FILTER/AGGREGATE VARIANTS to mutations.csv
@@ -79,7 +79,7 @@ mkdir /.tmp
 cat ${DIR_SCRIPT}/TL/TL_${SAMPLE}_${TL} \\
 | parallel --progress --jobs 2 gatk --java-options "\'-Xmx1G\'" AddOrReplaceReadGroups \\
 -I ${scBAM}/${SAMPLE}_{}.bam \\
--O /.tmp/${SAMPLE}_{}_UMI_SM.bam \\
+-O .tmp/${SAMPLE}_{}_UMI_SM.bam \\
 -ID ${SAMPLE}_{} \\
 -LB MissingLibrary \\
 -PL ILLUMINA \\
@@ -88,29 +88,29 @@ cat ${DIR_SCRIPT}/TL/TL_${SAMPLE}_${TL} \\
 
 # cat ${DIR_SCRIPT}/TL/TL_${SAMPLE}_${TL} \\
 # | parallel --progress --jobs 2 samtools index \\
-# /.tmp/${SAMPLE}_{}_UMI_SM.bam
+# .tmp/${SAMPLE}_{}_UMI_SM.bam
 
 cat ${DIR_SCRIPT}/TL/TL_${SAMPLE}_${TL} \\
 | parallel --jobs 2 gatk --java-options "\'-Xmx8g -XX:+UseConcMarkSweepGC\'" SplitNCigarReads \\
 -R ${REF} \\
--I /.tmp/${SAMPLE}_{}_UMI_SM.bam \\
--O /.tmp/${SAMPLE}_{}_UMI_SM_ST.bam
+-I .tmp/${SAMPLE}_{}_UMI_SM.bam \\
+-O .tmp/${SAMPLE}_{}_UMI_SM_ST.bam
 
 cat ${DIR_SCRIPT}/TL/TL_${SAMPLE}_${TL} \\
 | parallel --jobs 2 gatk --java-options "\'-Xmx8g -XX:+UseConcMarkSweepGC\'" Mutect2 \\
 -R ${REF_VAR}/${SAMPLE}_SM_bwa_RawSNPs_FLTR_SNP_consensus.fa \\
--I /.tmp/${SAMPLE}_{}_UMI_SM_ST.bam \\
+-I .tmp/${SAMPLE}_{}_UMI_SM_ST.bam \\
 -I ${BIGBAM} \\
 -tumor ${SAMPLE}_{} \\
 -normal ${SAMPLE}_combined \\
 -DF MappingQualityAvailableReadFilter \\
 -DF MappingQualityReadFilter \\
 -DF MappingQualityNotZeroReadFilter \\
--O /.tmp/${SAMPLE}_{}_var.vcf
+-O .tmp/${SAMPLE}_{}_var.vcf
 
 cat ${DIR_SCRIPT}/TL/TL_${SAMPLE}_${TL} \\
 | parallel --jobs 2 gatk --java-options "\'-Xmx4g -XX:+UseConcMarkSweepGC\'" FilterMutectCalls \\
--V /.tmp/${SAMPLE}_{}_var.vcf \\
+-V .tmp/${SAMPLE}_{}_var.vcf \\
 -O ${DIR_O}/${SAMPLE}_{}_var_FLTR.vcf \\
 --tumor-lod 5.3 \\
 --disable-tool-default-read-filters \\
@@ -122,14 +122,14 @@ LEN=$(wc -w < ${DIR_SCRIPT}/TL/TL_${SAMPLE}_${TL})
 
 for i in $( seq 1 ${LEN} ); do
 TAG=`awk "NR==$i" ${DIR_SCRIPT}/TL/TL_${SAMPLE}_${TL}`
-VART=$(cat /.tmp/${SAMPLE}_${TAG}_var.vcf | wc -l)
+VART=$(cat .tmp/${SAMPLE}_${TAG}_var.vcf | wc -l)
 VAR=$(cat  ${DIR_O}/${SAMPLE}_${TAG}_var_FLTR.vcf | grep "PASS" | wc -l)
 READ_SAM=$(samtools view -c -F 4 ${scBAM}/${SAMPLE}_${TAG}.bam)
 UMI=$(samtools view  ${scBAM}/${SAMPLE}_${TAG}.bam | grep -o \'UB:............\' | grep -o \'..........$\' | uniq -c | wc -l)
 COV=$(samtools mpileup ${scBAM}/${SAMPLE}_${TAG}.bam | awk -v X="${MIN_COVERAGE_DEPTH}" \'$4>=X\' | wc -l)
 echo $SAMPLE, $TAG, $VART, $VAR, $READ_SAM, $UMI, $COV >> ${DIR_O}/mutations.csv
 done
-rm -r /.tmp')
+rm -r .tmp')
     
 write(bash, file = paste(scripts_dir, '/VARIANTS_', sam, '_', ls, '.bash', sep = ''))
     
