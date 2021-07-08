9a5a197a945907422d6e33cd07753f806f9a1777
JeffreyCifello <jcifello1@gmail.com>
2021-07-08 18:12:48 -0400

    Update submit10_DonorBamStats.bash

--- step10/submit10_DonorBamStats.bash
+++ step10/submit10_DonorBamStats.bash
@@ -1,16 +1,26 @@
 #!/bin/bash
 
-MIN_COVERAGE_DEPTH=1
+donor_list=${sample:def_donor_list}
+exome_sc=${exome_sc:def_exome_sc}
+output_summary=${output_summary:def_output_summary}
 
-LEN=$(wc -w < /gpfs/gsfs5/users/TCR/10X_Genomics/code/test/mutations_cross/donor_list)
-FPATH=/gpfs/gsfs5/users/TCR/10X_Genomics/code/test/mutations_cross_out/1_aligned
+while [ $# -gt 0 ]; do            
+    if [[ $1 == *"--"* ]]; then
+      param="${1/--/}"
+      declare $param="$2"
+      echo $1 $2 #// Optional to see the parameter:value result
+    fi       
+  shift
+done
+
+LEN=$(wc -w < ${donor_list})
 
 for i in $( seq 1 ${LEN} ); do
 echo $i
 TAG=`awk "NR==$i" /gpfs/gsfs5/users/TCR/10X_Genomics/code/test/mutations_cross/donor_list`
-READ_EXOME=$(samtools view -c -F 4 -L /gpfs/gsfs5/users/TCR/10X_Genomics/code/test/mutations_cross/targets_chr.bed ${FPATH}/${TAG}_SM_bwa.bam)
-READ_SAM=$(samtools view -c -F 4 ${FPATH}/${TAG}_SM_bwa.bam)
-COV_EXOME=$(samtools depth -b /gpfs/gsfs5/users/TCR/10X_Genomics/code/test/mutations_cross/targets_chr.bed ${FPATH}/${TAG}_SM_bwa.bam | wc -l)
-COV=$(samtools depth ${FPATH}/${TAG}_SM_bwa.bam | wc -l)
-echo $TAG, $READ_EXOME, $READ_SAM, $COV_EXOME, $COV >> ./donor_summary.csv
+READ_EXOME=$(samtools view -c -F 4 -L /targets_chr.bed ${exome_sc}/${TAG}_SM_bwa.bam)
+READ_SAM=$(samtools view -c -F 4 ${exome_sc}/${TAG}_SM_bwa.bam)
+COV_EXOME=$(samtools depth -b /targets_chr.bed ${exome_sc}/${TAG}_SM_bwa.bam | wc -l)
+COV=$(samtools depth ${exome_sc}/${TAG}_SM_bwa.bam | wc -l)
+echo $TAG, $READ_EXOME, $READ_SAM, $COV_EXOME, $COV >> ${output_summary}
 done
