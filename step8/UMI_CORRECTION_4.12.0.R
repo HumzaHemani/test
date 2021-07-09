35ef8d152cdbedaa16783b88fdea6a4bcd74a675
JeffreyCifello <jcifello1@gmail.com>
2021-07-09 17:22:50 -0400

    Update UMI_CORRECTION_4.12.0.R

--- step8/UMI_CORRECTION_4.12.0.R
+++ step8/UMI_CORRECTION_4.12.0.R
@@ -10,6 +10,7 @@ library(tidyverse)
 # Arguments: metadata: tibble, mutations: tibble
 #sc_metadata <- read_csv(args[1])
 SingleCellMutations <- read_csv(args[1])
+tl_dir <- args[2]
 
 # test if there is at least one argument: if not, return an error; 
 # check if the arguments are of the proper type
@@ -20,4 +21,4 @@ if (length(args)==0) {
 }
 
 Args <- as.vector(rbind(SingleCellMutations$Chr,SingleCellMutations$POS,SingleCellMutations$bc,SingleCellMutations$POS))
-write(Args, file = 'UnfilteredMutations')
+write(Args, file = file.path(tl_dir, 'UnfilteredMutations'))
