a3d59b855eb5d3981331929750e2f9d555fc363c
JeffreyCifello <jcifello1@gmail.com>
2021-07-09 17:29:33 -0400

    Update step8.bash

--- step8/step8.bash
+++ step8/step8.bash
@@ -4,6 +4,7 @@ sample=${sample:default_sample}
 scBAM_dir=${scBAM_dir:default_scBAM_dir} # scBAM for a single cell. 
 mutations_csv=${mutations_csv:default_mutations_csv} # reformatted mutations output of step 7 for a single cell. 
 out_dir=${out_dir:default_out_dir}
+num_cores=${num_cores:default_num_cores}
 
 while [ $# -gt 0 ]; do            
     if [[ $1 == *"--"* ]]; then
@@ -33,18 +34,18 @@ Rscript /UMI_CORRECTION_4.12.0.R ${mutations_csv} ${out_dir}/TL
 # EXTRACT METADATA FOR CELL MUTATIONS:
 
 cat ${out_dir}/TL/UnfilteredMutations \
-| parallel --jobs=30 --max-args=4 samtools index ${DATA}/${SAMPLE}/${SAMPLE}_{3}-1.bam
+| parallel --jobs=${num_cores} --max-args=4 samtools index ${DATA}/${SAMPLE}/${SAMPLE}_{3}-1.bam
 
 samtools index ${scBAM}
 
 cat ${out_dir}/TL/UnfilteredMutations \
-| parallel --jobs=30 --max-args=4 samtools view -b -S -h ${scBAM_dir}/${sample}_{3}-1.bam {1}:{2}-{2} \
+| parallel --jobs=${num_cores} --max-args=4 samtools view -b -S -h ${scBAM_dir}/${sample}_{3}-1.bam {1}:{2}-{2} \
 '|' java -jar /jvarkit/dist/sam2tsv.jar \
 '|' grep -e {4} \
 '>>' ${out_dir}/${sample}_reads.tsv
 
 cat ${out_dir}/TL/UnfilteredMutations \
-| parallel --jobs=30 --max-args=4 samtools view ${scBAM_dir}/${sample}_{3}-1.bam {1}:{2}-{2} \
+| parallel --jobs=${num_cores} --max-args=4 samtools view ${scBAM_dir}/${sample}_{3}-1.bam {1}:{2}-{2} \
 | extract_meta \
 >> ${out_dir}/${sample}_meta.tsv
 
