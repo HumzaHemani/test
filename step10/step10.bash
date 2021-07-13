6c9241da1cf05905da38e9a6ae63066a21a5adf3
JeffreyCifello <jcifello1@gmail.com>
2021-07-13 07:31:24 -0400

    Update and rename submit10_DonorBamStats.bash to step10.bash

--- step10/step10.bash
+++ step10/step10.bash
@@ -2,7 +2,7 @@
 
 donor_list=${sample:def_donor_list}
 exome_sc=${exome_sc:def_exome_sc}
-output_summary=${output_summary:def_output_summary}
+output_dir=${output_dir:def_output_dir}
 
 while [ $# -gt 0 ]; do            
     if [[ $1 == *"--"* ]]; then
@@ -17,10 +17,10 @@ LEN=$(wc -w < ${donor_list})
 
 for i in $( seq 1 ${LEN} ); do
 echo $i
-TAG=`awk "NR==$i" /gpfs/gsfs5/users/TCR/10X_Genomics/code/test/mutations_cross/donor_list`
+TAG=`awk "NR==$i" ${donor_list}`
 READ_EXOME=$(samtools view -c -F 4 -L /targets_chr.bed ${exome_sc}/${TAG}_SM_bwa.bam)
 READ_SAM=$(samtools view -c -F 4 ${exome_sc}/${TAG}_SM_bwa.bam)
 COV_EXOME=$(samtools depth -b /targets_chr.bed ${exome_sc}/${TAG}_SM_bwa.bam | wc -l)
 COV=$(samtools depth ${exome_sc}/${TAG}_SM_bwa.bam | wc -l)
-echo $TAG, $READ_EXOME, $READ_SAM, $COV_EXOME, $COV >> ${output_summary}
+echo $TAG, $READ_EXOME, $READ_SAM, $COV_EXOME, $COV >> ${output_dir}/summary.csv
 done
